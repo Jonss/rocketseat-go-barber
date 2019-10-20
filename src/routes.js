337b2d11098259c32a746e11bcf6c4a3a4293e6c
Joao Santana <joaosantana.ti@gmail.com>
2019-10-20 13:45:07 -0300

    Creating db connection and persistig first user for test

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Hello Go Barber!' }));
+
+routes.post('/user', async (req, res) => {
+  const user = await User.create({
+    name: 'João Santana',
+    email: 'joao.santana@gmail.com',
+    password_hash: 'jajajajaja',
+  });
+
+  return res.json(user);
+});
 
 export default routes;

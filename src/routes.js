7e54d56e2ce7d6bab33abc74347034cd2c549777
Joao Santana <joaosantana.ti@gmail.com>
2019-10-22 20:40:45 -0300

    Saving image url and responding in provider endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import multer from 'multer';
-import multerConfig from './config/multer';
 
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,8 +20,8 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;

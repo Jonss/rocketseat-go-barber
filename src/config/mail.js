c6ae929a04eb939a575bcb88d9dadb6327b8ab9f
Joao Santana <joaosantana.ti@gmail.com>
2019-10-24 21:13:53 -0300

    Sending email with nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '6ddc568f7fd105',
+    pass: 'd0454016cae786',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
